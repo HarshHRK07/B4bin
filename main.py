07dc8d41e6ad391971e7911d2fc44144f853477f
HarshHRK07 <149139740+HarshHRK07@users.noreply.github.com>
2024-08-19 01:56:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 # Telegram bot token and chat ID
 BOT_TOKEN = "7195510626:AAEyR3F9NH5cyMsEcObqCpFvkwCFrc6C9M4"
-CHAT_ID = "6460703454"
+CHAT_ID = " -1002163921366"
 
 app = Flask(__name__)
 
